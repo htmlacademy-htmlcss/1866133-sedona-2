59a1931ed029f1baf3f3dd493a98137b6d6ec03e
SeoVoodoo <91605041+SeoVoodoo@users.noreply.github.com>
2025-05-20 09:36:22 +0400

    Открытие и закрытие модального окна

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+const searchHostelBtn = document.querySelector(".btn-search");
+const modalWindow =  document.querySelector(".modal-container");
+const closeBtn =  document.querySelector(".modal-close-button");
 
+searchHostelBtn.addEventListener("click", function(){
+  modalWindow.classList.remove("modal-container-close");
+})
+
+closeBtn.addEventListener("click", function(){
+  modalWindow.classList.add("modal-container-close");
+})
+
+const handleKeydown = (e) => {
+  if(e.keyCode === 27) {
+    modalWindow.classList.add("modal-container-close");
+  }
+}
+
+document.addEventListener('keydown', handleKeydown);
