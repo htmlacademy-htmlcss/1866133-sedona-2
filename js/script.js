a6a6f651dcdc5441de59e14033aef773ab354eb5
SeoVoodoo <91605041+SeoVoodoo@users.noreply.github.com>
2025-05-21 18:48:27 +0400

    Отменяет прокурутку при открытии модального окна

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
+const body = document.querySelector("body");
 const searchHostelBtn = document.querySelector(".btn-search");
 const modalWindow =  document.querySelector(".modal-container");
 const closeBtn =  document.querySelector(".modal-close-button");
 
+
 searchHostelBtn.addEventListener("click", function(){
+  // const scrollY = window.scrollY || document.documentElement.scrollTop;
+  // modalWindow.style.setProperty('--scroll-position', `${scrollY}px`);
+  // body.classList.add("no-scroll");
+  body.style.overflow = "hidden";
   modalWindow.classList.remove("modal-container-close");
+
 })
 
 closeBtn.addEventListener("click", function(){
   modalWindow.classList.add("modal-container-close");
+  body.style.overflow = "";
 })
 
 const handleKeydown = (e) => {
